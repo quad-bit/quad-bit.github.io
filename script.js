afbed7043f838b8ef662946942875292c4e63981
Prashant Ravindran <prashant_ravindran@hotmail.com>
2026-08-22 14:26:54 +0530

    Core page templates
    Home, timeline, craft and contact templates

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// var option = 0;
+
+// // Select the link using its ID
+// const link = document.getElementById('timeline');
+
+// // Listen for the click event
+// link.addEventListener('click', function(event) 
+// {
+//     // Optional: Stop the link from opening the URL immediately
+//     //event.preventDefault(); 
+    
+//     link.classList.toggle('highlight');
+//     console.log('The link was clicked!');
+    
+//     // Optional: Manually redirect the user later
+//     // window.location.href = this.href; 
+// });
